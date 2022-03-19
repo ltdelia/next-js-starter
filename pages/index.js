642870d7859888482dcdf37404be0f9783fa172b
Louis Delia <delia.louis@gmail.com>
2022-03-19 17:57:21 -0400

    removing out dir, reconfiguring netlify.toml

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,39 @@
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { useState, useEffect } from 'react';
-
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
 function Home() {
+  const [paths, setPaths] = useState([]);
 
-    const [paths, setPaths] = useState([]);
-
-    useEffect(() => {
-      if(paths.length === 0){
-        generatePaths();
-      }
-    })
-
-    const generatePaths = async () => {
-      const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
-      const pokemon = await res.json();
-    
-      let paths = pokemon.results.map(p => {
-        return `/pokemon/${p.name}`
-      })
-
-      setPaths(paths);
-    }    
-
-    const router = useRouter() 
-
-    const handleClick = (ev) => {
-        ev.preventDefault();
-        router.push(paths[Math.floor(Math.random() * paths.length)]);
+  useEffect(() => {
+    if (paths.length === 0) {
+      generatePaths();
     }
+  });
+
+  const generatePaths = async () => {
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+    const pokemon = await res.json();
+
+    let paths = pokemon.results.map((p) => {
+      return `/pokemon/${p.name}`;
+    });
+
+    setPaths(paths);
+  };
+
+  const router = useRouter();
+
+  const handleClick = (ev) => {
+    ev.preventDefault();
+    router.push(paths[Math.floor(Math.random() * paths.length)]);
+  };
 
   return (
     <>
-        <Head>
-            <title>My Next.js site</title>
-        </Head>
+      <Head>
+        <title>My Next.js site</title>
+      </Head>
       <div>Welcome Explorer!</div>
       <button onClick={handleClick}>Click Me!</button>
     </>
