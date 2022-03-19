bd5e2e1eda0f3540a38b693c780dc2b42a9187ff
Louis Delia <delia.louis@gmail.com>
2022-03-19 16:00:23 -0400

    linking to a random pokemon page on button click

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,36 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
 
 
 function Home() {
+
+    const [paths, setPaths] = useState([]);
+
+    useEffect(() => {
+      if(paths.length === 0){
+        generatePaths();
+      }
+    })
+
+    const generatePaths = async () => {
+      const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+      const pokemon = await res.json();
+    
+      let paths = pokemon.results.map(p => {
+        return `/pokemon/${p.name}`
+      })
+
+      setPaths(paths);
+    }    
 
     const router = useRouter() 
 
     const handleClick = (ev) => {
         ev.preventDefault();
-        router.push('/pokemon');
+        router.push(paths[Math.floor(Math.random() * paths.length)]);
     }
+
   return (
     <>
         <Head>
