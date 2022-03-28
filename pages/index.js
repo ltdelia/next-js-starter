ab14632519a138c067477d7fe35cede59b4e8fdc
Louis Delia <delia.louis@gmail.com>
2022-03-27 21:13:22 -0400

    removing pokemon pages

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
 
 function Home() {
-  const [paths, setPaths] = useState([]);
-
-  useEffect(() => {
-    if (paths.length === 0) {
-      generatePaths();
-    }
-  });
-
-  const generatePaths = async () => {
-    const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
-    const pokemon = await res.json();
-
-    let paths = pokemon.results.map((p) => {
-      return `/pokemon/${p.name}`;
-    });
-
-    setPaths(paths);
-  };
-
-  const router = useRouter();
-
-  const handleClick = (ev) => {
-    ev.preventDefault();
-    router.push(paths[Math.floor(Math.random() * paths.length)]);
-  };
-
-  if (router.isFallback) {
-    return <div>Loading...</div>
-}
-
   return (
     <>
       <Head>
@@ -43,7 +11,6 @@ function Home() {
       <Link href="/blog">
         <a>Visit the Blog Page!</a>
       </Link>
-      <button onClick={handleClick}>Get a random Pokemon!</button>
     </>
   );
 }
